1d1d7ab633aeb301e14a055152a88672a065ba16
Ahmed Khalil <amraniahmedkhalil@gmail.com>
2023-03-17 14:04:42 +0100

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
 export default function Home() {
   return (
-    <main>
-      <h1>Hello, World!</h1>
-    </main>
+    <section className=" flex justify-center items-center h-screen w-screen bg-[#F0F2F5]">
+      <div className="">
+        <div>
+          <h1>this d1to test branch</h1>
+          <h1>FaceNotebook</h1>
+          <h3>Connect with friends and the world around you on Facebook.</h3>
+        </div>
+        <div>
+          <h1>Login</h1>
+        </div>
+      </div>
+    </section>
   )
 }
