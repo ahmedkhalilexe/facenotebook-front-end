340003acc40c1382be3fff2c50b99dc1215a26e6
Ahmed Khalil <amraniahmedkhalil@gmail.com>
2023-03-17 16:01:22 +0100

    added resposinve

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="debug-screens">{children}</body>
     </html>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,10 @@ import Login from "./components/loginform";
 
 export default function Home() {
   return (
-    <section className=" flex justify-center items-center h-screen w-screen bg-[#F0F2F5]">
-      <div className="flex w-2/3 flex-row h-3/5 justify-between gap-10">
-        <div className=" basis-1/2 w-fit mt-20">
-          <h1 className=" text-4xl font-bold text-blue-700 mb-2">FaceNotebook</h1>
+    <section className=" flex justify-center lg:items-center  h-screen w-screen bg-[#F0F2F5]">
+      <div className="flex w-2/3 flex-col lg:flex-row h-3/5 justify-between gap-10">
+        <div className=" basis-1/2 w-fit mt-11  lg:mt-20 text-center lg:text-left">
+          <h1 className=" text-5xl font-bold text-blue-700 mb-2">FaceNotebook</h1>
           <h3 className=" text-2xl">Connect with friends and the world around you on FaceNotebook.</h3>
         </div>
         <div className="basis-1/2">
